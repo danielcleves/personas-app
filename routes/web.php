b7b41f6f77a549e543243c8ad35391ff60b46ed3
Daniel Cleves <daniel.cleves.rueda@gmail.com>
2025-03-24 00:01:16 -0500

    ✨ feat: Añadir rutas para gestionar municipios en el controlador

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Comunas
 Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
 Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
 Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
 Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
 Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
 Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
+
+// Municipios
+Route::get('/municipios', [ComunaController::class, 'index'])->name('municipios.index');
+Route::post('/municipios', [ComunaController::class, 'store'])->name('municipios.store');
+Route::get('/municipios/create', [ComunaController::class, 'create'])->name('municipios.create');
+Route::delete('/municipios/{comuna}', [ComunaController::class, 'destroy'])->name('municipios.destroy');
+Route::put('/municipios/{comuna}', [ComunaController::class, 'update'])->name('municipios.update');
+Route::get('/municipios/{comuna}/edit', [ComunaController::class, 'edit'])->name('municipios.edit');
